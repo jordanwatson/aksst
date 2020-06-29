1b6dc6a4f2ad7da0229632d66c6cfe26739b8450
Jordan Watson <jordanwatson@users.noreply.github.com>
2020-06-29 11:19:19 -0800

    Init commit

---
+++ R/plot_ak_sst_generic.R
@@ -0,0 +1,87 @@
+#  Create plotting function that will allow selection of 2 ESR regions
+plot_ak_sst_generic <- function(region1,region2){
+
+  #  Load 508 compliant NOAA colors
+  OceansBlue1='#0093D0'
+  OceansBlue2='#0055A4' # rebecca dark blue
+  CoralRed1='#FF4438'
+  SeagrassGreen1='#93D500'
+  SeagrassGreen4='#D0D0D0' # This is just grey
+  UrchinPurple1='#7F7FFF'
+  WavesTeal1='#1ECAD3'
+
+  #  Assign colors to different time series.
+  current.year.color <- OceansBlue1
+  last.year.color <- WavesTeal1
+  mean.color <- "black"
+
+  #  Set default plot theme
+  theme_set(theme_cowplot())
+
+  #  Specify legend position coordinates
+  mylegx <- 0.525
+  mylegy <- 0.865
+
+
+  #  Set year criteria to automatically identify the current and previous years
+  current.year <- max(data$year)
+  last.year <- current.year-1
+  mean.years <- 2003:2012
+  mean.lab <- "Mean 2003-2012"
+
+  mylines_base <- ggplot() +
+    geom_line(data=data %>% filter(year2<last.year & esr_region%in%(c(region1,region2))),
+              aes(newdate,meansst,group=factor(year2),col='mygrey'),size=0.3) +
+    geom_line(data=data %>% filter(year2==last.year & esr_region%in%(c(region1,region2))),
+              aes(newdate,meansst,color='last.year.color'),size=0.75) +
+    geom_line(data=data %>%
+                filter(year%in%mean.years & esr_region%in%(c(region1,region2))) %>%
+                group_by(esr_region2,newdate) %>%
+                summarise(meantemp=mean(meansst,na.rm=TRUE)),
+              aes(newdate,meantemp,col='mean.color'),size=0.5) +
+    geom_line(data=data %>% filter(year2==current.year & esr_region%in%(c(region1,region2))),
+              aes(newdate,meansst,color='current.year.color'),size=0.95) +
+    facet_wrap(~esr_region2,ncol=2) +
+    scale_color_manual(name="",
+                       breaks=c('current.year.color','last.year.color','mygrey','mean.color'),
+                       values=c('current.year.color'=current.year.color,'last.year.color'=last.year.color,'mygrey'=SeagrassGreen4,'mean.color'=mean.color),
+                       labels=c(current.year,last.year,paste0('2002-',last.year-1),mean.lab)) +
+    ylab("Mean Sea Surface Temperature (C)") +
+    xlab("") +
+    scale_x_date(date_breaks="1 month",
+                 date_labels = "%b",
+                 expand = c(0.025,0.025)) +
+    theme(legend.position=c(mylegx,mylegy),
+          legend.text = element_text(size=8,family="sans"),
+          #legend.background = element_rect(fill="white"),
+          legend.background = element_blank(),
+          legend.title = element_blank(),
+          strip.text = element_text(size=10,color="white",family="sans",face="bold"),
+          strip.background = element_rect(fill=OceansBlue2),
+          axis.title = element_text(size=10,family="sans"),
+          panel.border=element_rect(colour="black",size=0.75),
+          axis.text.x=element_text(color=c("black",NA,NA,"black",NA,NA,"black",NA,NA,"black",NA,NA,NA)),
+          legend.key.size = unit(0.35,"cm"),
+          plot.margin=unit(c(0.65,0,0.65,0),"cm"))
+
+
+ggdraw(mylines_base)
+
+}
+
+#png(paste0("figure_output/SST_Twitter_",format(Sys.Date(),"%Y_%m_%d"),".png"),width=6,height=3.375,units="in",res=300)
+#myplotfun("NBS","EBS")
+#dev.off()
+#myplotfun("EGOA","WGOA")
+
+#jpeg(paste0("figure_output/SST_Bering_Twitter_",format(Sys.Date(),"%Y_%m_%d"),".jpeg"),width=6,height=4,units="in",quality=100,res=300)
+#myplotfun("NBS","EBS")
+#dev.off()
+
+#jpeg(paste0("figure_output/SST_GOA_Twitter_",format(Sys.Date(),"%Y_%m_%d"),".jpeg"),width=6,height=4,units="in",quality=100,res=300)
+#myplotfun("EGOA","WGOA")
+#dev.off()
+
+#  Download heatmap of Alaska region
+#link = "https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41anom1day.largePng?sstAnom%5B(2020-06-15T09:00:00Z)%5D%5B(50.0):(70.0)%5D%5B(-174.99):(-141.0)%5D&.draw=surface&.vars=longitude%7Clatitude%7CsstAnom&.colorBar=%7C%7C%7C%7C%7C&.bgColor=0xffccccff"
+#download.file(link,destfile=paste0("Heatmap_",format(Sys.Date(),"%Y_%m_%d"),".png"),mode='wb')
---
+++ ReadME.md
@@ -0,0 +1,40 @@
+
+
+Author: Jordan Watson (jordan.watson@noaa.gov)
+
+
+This package uses the JPL MUR SST dataset to plot regional Alaska sea surface temperatures. The data are downloaded daily through the CoastWatch West Coast Node ERDDAP, and ingested at AKFIN, the Alaska Fisheries Information Network. For each day, approximately 10 million temperature records are aggregated into the Alaska Department of Fish and Game statistical management areas. Within each of these areas and on each day, the data are averaged to yield 1,758 daily average SST records. These data are then binned into regional spatial strata as described by the NOAA Alaska Fisheries Science Center Ecosystem Status Consideration spatial strata, "Eastern Bering Sea (EBS)", "Northern Bering Sea (NBS)", "Western Gulf of Alaska (WGOA)", and the "Eastern Gulf of Alaska (EGOA)". These daily regional SST values are available via a public webAPI at AKFIN. The key step to this R package is to query this daily time series from AKFIN using this API key, and this is what is downloaded when using the `update_sst_data()` function.
+
+
+The aksst package can be installed for R from github.
+
+```
+library(devtools)
+install_github("jordanwatson/aksst")
+```
+
+Currently there are only a few basic functions. The data are updated daily and the full time series is updated when running the `update_sst_data()` function. A message will notify users that this may take 10-20 seconds to download the data.
+
+```
+library(aksst)
+data <- update_sst_data()
+```
+There are four regions that can be easily examined, "EBS", "NBS", "EGOA", "WGOA". The scaling works best if Bering Sea plots are kept together and GOA plots are kept together. Current formatting is for two areas at a time. A future version may include a single area.
+
+A single function will output a formatted jpg file to the working directory, whith the NOAA logo and some metadata at the bottom.
+```
+plot_ak_sst("EBS","NBS")
+```
+
+To plot a generic version to your R console, you can use the generic plotting function that omits the NOAA logo and metadata.
+```
+plot_ak_sst_generic("EBS","NBS")
+```
+
+
+
+
+For more information on the spatial strata, see descriptions in the Bering Sea and Gulf of Alaska Ecosystem Status Reports (keyword search for Watson).
+https://access.afsc.noaa.gov/REFM/REEM/ecoweb/
+
+For more information about methods
